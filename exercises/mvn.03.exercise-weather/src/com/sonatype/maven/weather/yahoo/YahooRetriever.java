e3e18c316c45e318cbd896ad3879668e0a3683eb
lofidewanto <lofidewanto@gmail.com>
2019-04-07 00:36:43 +0200

    add alternative implementation if you don't have internet connection

--- exercises/mvn.03.exercise-weather/src/com/sonatype/maven/weather/yahoo/YahooRetriever.java
+++ exercises/mvn.03.exercise-weather/src/com/sonatype/maven/weather/yahoo/YahooRetriever.java
@@ -1,6 +1,8 @@
 package com.sonatype.maven.weather.yahoo;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -12,9 +14,20 @@ public class YahooRetriever {
 
 	public InputStream retrieve(String zipcode) throws Exception {
 		log.info("Retrieving Weather Data");
+		InputStream inputStream = getFromFile(zipcode);
+		return inputStream;
+	}
+
+	@SuppressWarnings("unused")
+	private InputStream getFromInternet(String zipcode) throws IOException, MalformedURLException {
 		String url = "http://weather.yahooapis.com/forecastrss?p=" + zipcode;
 		URLConnection conn = new URL(url).openConnection();
 		return conn.getInputStream();
+	}
+	
+	private InputStream getFromFile(String zipcode) throws IOException, MalformedURLException {
+		InputStream nyData = getClass().getClassLoader().getResourceAsStream("ny-weather.xml");
+		return nyData;
 	}
 
 }
